b55bb86a1752825e50d390ae23b283314d196c2f
Rostislav <rostislaved@gmail.com>
2022-12-18 04:36:53 +0800

    refactor,add timeout,remove chinese

--- main.go
+++ main.go
@@ -6,66 +6,95 @@ import (
 	"fmt"
 	"log"
 	"os"
+	"strings"
+	"time"
 
-	gpt3 "github.com/PullRequestInc/go-gpt3"
+	"github.com/PullRequestInc/go-gpt3"
 	"github.com/spf13/cobra"
 )
-
-func GetResponse(client gpt3.Client, ctx context.Context, quesiton string) {
-	err := client.CompletionStreamWithEngine(ctx, gpt3.TextDavinci003Engine, gpt3.CompletionRequest{
-		Prompt: []string{
-			quesiton,
-		},
-		MaxTokens:   gpt3.IntPtr(3000),
-		Temperature: gpt3.Float32Ptr(0),
-	}, func(resp *gpt3.CompletionResponse) {
-		fmt.Print(resp.Choices[0].Text)
-	})
-	if err != nil {
-		fmt.Println(err)
-		os.Exit(13)
-	}
-	fmt.Printf("\n")
-}
-
-type NullWriter int
-
-func (NullWriter) Write([]byte) (int, error) { return 0, nil }
 
 func main() {
 	log.SetOutput(new(NullWriter))
 	apiKey := os.Getenv("API_KEY")
 	if apiKey == "" {
-		panic("Missing API KEY")
+		panic("Missing API_KEY")
 	}
 
 	ctx := context.Background()
+
 	client := gpt3.NewClient(apiKey)
+
 	rootCmd := &cobra.Command{
 		Use:   "chatgpt",
 		Short: "Chat with ChatGPT in console.",
 		Run: func(cmd *cobra.Command, args []string) {
 			scanner := bufio.NewScanner(os.Stdin)
-			quit := false
 
-			for !quit {
-				fmt.Print("輸入你的問題(quit 離開): ")
+			for {
+				fmt.Print(">> ")
 
 				if !scanner.Scan() {
 					break
 				}
 
 				question := scanner.Text()
+
 				switch question {
-				case "quit":
-					quit = true
+				case "quit", "exit":
+					return
 
 				default:
-					GetResponse(client, ctx, question)
+					func() {
+						ctx, cancel := context.WithTimeout(ctx, 10*time.Second)
+						defer cancel()
+
+						response, err := getResponse(ctx, client, question)
+						if err != nil {
+							fmt.Println(err)
+						}
+
+						fmt.Println()
+						fmt.Println(response)
+					}()
 				}
 			}
 		},
 	}
 
 	rootCmd.Execute()
+}
+
+func getResponse(ctx context.Context, client gpt3.Client, question string) (response string, err error) {
+	sb := strings.Builder{}
+
+	err = client.CompletionStreamWithEngine(
+		ctx,
+		gpt3.TextDavinci003Engine,
+		gpt3.CompletionRequest{
+			Prompt: []string{
+				question,
+			},
+			MaxTokens:   gpt3.IntPtr(3000),
+			Temperature: gpt3.Float32Ptr(0),
+		},
+		func(resp *gpt3.CompletionResponse) {
+			text := resp.Choices[0].Text
+
+			sb.WriteString(text)
+		},
+	)
+	if err != nil {
+		return "", err
+	}
+
+	response = sb.String()
+	response = strings.TrimLeft(response, "\n")
+
+	return response, nil
+}
+
+type NullWriter int
+
+func (NullWriter) Write([]byte) (int, error) {
+	return 0, nil
 }
